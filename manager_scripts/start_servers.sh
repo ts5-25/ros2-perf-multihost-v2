b4d1dad97c9256046ebc48e68c92c7a7adfc5a99
ts5-25 <yongwutianzhong@gmail.com>
2025-12-26 16:38:14 +0900

    host名

--- manager_scripts/start_servers.sh
+++ manager_scripts/start_servers.sh
@@ -4,5 +4,5 @@ HOSTS=("pi0" "pi1" "pi2" "pi3" "pi4") # 必要なホスト名に調整
 
 for host in "${HOSTS[@]}"; do
   echo "Starting REST server on $host"
-  ssh "$host" 'source /home/pi/ros2-perf-multihost-v2/.venv/bin/activate && nohup python /home/pi/ros2-perf-multihost-v2/manager_scripts/manager_scripts.py > /home/pi/rest.log 2>&1 &'
+  ssh "$host" 'source /home/ubuntu/ros2-perf-multihost-v2/.venv/bin/activate && nohup python /home/ubuntu/ros2-perf-multihost-v2/manager_scripts/manager_scripts.py > /home/ubuntu/rest.log 2>&1 &'
 done
