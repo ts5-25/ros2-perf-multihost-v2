b04ee78a0b754fdf38cf1e23edd786fa7f814bd5
ts5-25 <yongwutianzhong@gmail.com>
2025-12-26 15:38:09 +0900

    setup script

---
+++ manager_scripts/start_servers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+HOSTS=("pi0" "pi1" "pi2" "pi3" "pi4") # 必要なホスト名に調整
+
+for host in "${HOSTS[@]}"; do
+  echo "Starting REST server on $host"
+  ssh "$host" 'nohup python3 /home/pi/ros2-perf-multihost-v2/manager_scripts/manager_scripts.py > /home/pi/rest.log 2>&1 &'
+done
